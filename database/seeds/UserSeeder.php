76a3f699de753ae841425fb9ce5c02bfbd736040
wilmar.rincon <wilmar.rincon@linktic.com>
2021-09-13 20:50:31 -0500

    Creacion de los roles de usuario

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -17,6 +17,7 @@ class UserSeeder extends Seeder
             'name' => 'Super Administrador',
             'email' => 'admin@gmail.com',
             'password' => bcrypt('123456789'),
+            'role_id' => '1',
         ]);
     }
 }
